871680f31f62e4c300b19cadc40108c8f9d55a60
Tushar C <tushar@alzaf.com>
2023-09-08 21:57:05 -0700

    Optimize

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::paginate();
+        $users = User::all();
 
         return view('users.index', compact('users'));
     }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,9 @@
         <div class="inline-flex overflow-hidden mb-4 w-full bg-white rounded-lg shadow-md">
             <div class="flex justify-center items-center w-12 bg-blue-500">
                 <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z"></path>
+                    <path
+                        d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z">
+                    </path>
                 </svg>
             </div>
 
@@ -24,28 +26,33 @@
             <div class="overflow-x-auto w-full">
                 <table class="w-full whitespace-no-wrap">
                     <thead>
-                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase bg-gray-50 border-b">
-                        <th class="px-4 py-3">Name</th>
-                        <th class="px-4 py-3">Email</th>
-                    </tr>
+                        <tr
+                            class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase bg-gray-50 border-b">
+                            <th class="px-4 py-3">Name</th>
+                            <th class="px-4 py-3">Email</th>
+                            <th class="px-4 py-3">Last Project</th>
+                        </tr>
                     </thead>
                     <tbody class="bg-white divide-y">
-                    @foreach($users as $user)
-                        <tr class="text-gray-700">
-                            <td class="px-4 py-3 text-sm">
-                                {{ $user->name }}
-                            </td>
-                            <td class="px-4 py-3 text-sm">
-                                {{ $user->email }}
-                            </td>
-                        </tr>
-                    @endforeach
+                        @foreach ($users as $user)
+                            <tr class="text-gray-700">
+                                <td class="px-4 py-3 text-sm">
+                                    {{ $user->name }}
+                                </td>
+                                <td class="px-4 py-3 text-sm">
+                                    {{ $user->email }}
+                                </td>
+                                <td class="px-4 py-3 text-sm">
+                                    {{ $user->lastProject->name }}
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
-            <div class="px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase bg-gray-50 border-t sm:grid-cols-9">
+            {{-- <div class="px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase bg-gray-50 border-t sm:grid-cols-9">
                 {{ $users->links() }}
-            </div>
+            </div> --}}
         </div>
 
     </div>
