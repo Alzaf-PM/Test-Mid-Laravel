8652364d75f783fc5d6f0060d218491b15a2281f
Tushar C <tushar@alzaf.com>
2023-09-09 02:54:14 -0700

    relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function parent()
+    {
+
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategories()
+    {
+
+        return $this->hasMany(Category::class);
+    }
 }
